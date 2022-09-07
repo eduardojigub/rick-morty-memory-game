bc882fdf11d682084777aa29c2ffb4ff044888b5
eduardojigub <eduardobrunoit@gmail.com>
2022-09-07 11:34:31 -0300

    creating cards dinamically

--- js/game.js
+++ js/game.js
@@ -1,23 +1,44 @@
 const grid = document.querySelector(".grid");
 
+const characters = [
+  "beth",
+  "jerry",
+  "jessica",
+  "morty",
+  "pessoa-passaro",
+  "pickle-rick",
+  "rick",
+  "summer",
+  "meeseeks",
+  "scroopy",
+];
+
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
   element.className = className;
   return element;
 };
 
-const createCard = () => {
+const createCard = (character) => {
   const card = createElement("div", "card");
   const front = createElement("div", "face front");
   const back = createElement("div", "face back");
+
+  front.style.backgroundImage = `url('../images/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
 
   grid.appendChild(card);
+
+  return card;
 };
 
-createCard();
-createCard();
-createCard();
-createCard();
+const loadGame = () => {
+  characters.forEach((character) => {
+    const card = createCard(character);
+    grid.appendChild(card);
+  });
+};
+
+loadGame();
