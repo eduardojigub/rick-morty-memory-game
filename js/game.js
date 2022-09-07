165b8da310c8ce9c97102a2f20fd825bb7e51d9b
eduardojigub <eduardobrunoit@gmail.com>
2022-09-07 12:15:37 -0300

    adding function CheckEndGame

--- js/game.js
+++ js/game.js
@@ -22,6 +22,17 @@ const createElement = (tag, className) => {
 let firstCard = "";
 let secondCard = "";
 
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll(".disabled-card");
+
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop);
+    alert(
+      `Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`
+    );
+  }
+};
+
 const checkCards = () => {
   const firstCharacter = firstCard.getAttribute("data-character");
   const secondCharacter = secondCard.getAttribute("data-character");
@@ -32,6 +43,8 @@ const checkCards = () => {
 
     firstCard = "";
     secondCard = "";
+
+    checkEndGame();
   } else {
     setTimeout(() => {
       firstCard.classList.remove("reveal-card");
