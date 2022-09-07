19b1f130e01783a4e958f6d5a79e11487ccd0459
eduardojigub <eduardobrunoit@gmail.com>
2022-09-07 12:33:11 -0300

    adding counter

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid");
 const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
 
 const characters = [
   "beth",
@@ -28,7 +29,9 @@ const checkEndGame = () => {
 
   if (disabledCards.length === 20) {
     clearInterval(this.loop);
-    alert(`Parabéns, você venceu`);
+    alert(
+      `Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`
+    );
   }
 };
 
@@ -99,7 +102,15 @@ const loadGame = () => {
   });
 };
 
+const startTimer = () => {
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+};
+
 window.onload = () => {
   spanPlayer.innerHTML = localStorage.getItem("player");
+  startTimer();
   loadGame();
 };
